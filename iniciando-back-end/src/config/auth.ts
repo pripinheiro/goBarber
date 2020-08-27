adbb44115d2ecfc6f8b1638a3f11aee56b6e577c
Pri Pinheiro <pripinheiro.sa@gmail.com>
2020-08-27 19:53:25 -0300

    authentication included

---
+++ iniciando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9ff8d1da2919af371dda3937d3151047',
+    expiresIn: '1d'
+  }
+}
--- iniciando-back-end/src/database/migrations/1598475350802-CreateUsers.ts
+++ iniciando-back-end/src/database/migrations/1598475350802-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1598464693509 implements MigrationInterface {
+export default class CreateUsers1598475350802 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -15,13 +15,19 @@ export default class CreateAppointments1598464693509 implements MigrationInterfa
                         default: 'uuid_generate_v4()',  
                     },
                     {
-                        name: 'provider',
-                        type: 'varchar',    
+                        name: 'name',
+                        type: 'varchar', 
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
                     },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },    
+
                     {
                         name:'created_at',
                         type: 'timestamp',
@@ -39,7 +45,7 @@ export default class CreateAppointments1598464693509 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
