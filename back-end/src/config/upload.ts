c9873155c494669fb4b0673a2cd51fde8fccd134
Pri Pinheiro <pripinheiro.sa@gmail.com>
2020-08-31 12:22:56 -0300

    handling exceptions

--- back-end/src/config/upload.ts
+++ back-end/src/config/upload.ts
@@ -1,10 +1,15 @@
 import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
+import { TreeParent } from 'typeorm';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
+  directory: tmpFolder,
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
