a53c990476701ce836f92f7addd278e51f0f60a4
Pri Pinheiro <pripinheiro.sa@gmail.com>
2020-09-25 10:02:14 -0300

    updated architecture

--- back-end/src/modules/users/services/AuthenticateUserService.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.ts
@@ -2,10 +2,10 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
-import AppError from '../errors/AppError';
-import authConfig from '../config/auth'
+import AppError from '@shared/errors/AppError';
+import authConfig from '@config/auth'
 
-import User from '../models/Users';
+import User from '../infra/typeorm/entities/Users';
 
 
 interface Request {
